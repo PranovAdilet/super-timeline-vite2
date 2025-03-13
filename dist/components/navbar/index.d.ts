117deba0fbc8df163c9e68da07e214232b3bbb77
PranovAdilet <pranov.adiletqwe@mail.ru>
2025-03-13 19:40:11 +0600

    feat: add HistoryButtons and DownloadPopover components to navbar; update type definitions

--- dist/components/navbar/index.d.ts
+++ dist/components/navbar/index.d.ts
@@ -1,4 +1,8 @@
 export default function Navbar(): import("react/jsx-runtime").JSX.Element;
 export declare const ResizeVideo: () => import("react/jsx-runtime").JSX.Element;
+export declare const HistoryButtons: () => import("react/jsx-runtime").JSX.Element;
+export declare const DownloadPopover: ({ onExport, }: {
+    onExport?: (data: any) => void;
+}) => import("react/jsx-runtime").JSX.Element;
 export declare const download: (url: string, filename: string) => void;
 //# sourceMappingURL=index.d.ts.map
