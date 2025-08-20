2495f10d611aaba4224eea4fac778446905ed432
PranovAdilet <pranov.adiletqwe@mail.ru>
2025-08-21 02:19:42 +0600

    Added export of composition component to player file. Updated imports to improve code structure.

--- dist/components/player/index.d.ts
+++ dist/components/player/index.d.ts
@@ -1,3 +1,4 @@
 export * from "./player";
 export * from "./root-composition";
+export * from "./composition";
 //# sourceMappingURL=index.d.ts.map
--- src/components/player/index.ts
+++ src/components/player/index.ts
@@ -1,2 +1,3 @@
 export * from "./player";
 export * from "./root-composition";
+export * from "./composition";
